c39ec8106fb96c0657be89dd571484f169b749ab
Alan Patriarca <alansantana@alunos.utfpr.edu.br>
2018-11-10 15:05:12 -0200

    Atualização Conexão com banco

---
+++ eleicao/src/dao/Config.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author Alan Patriarca
+ */
+public class Config {
+    public static final String URL = "jdbc:mysql://localhost/F1?useSSL=false";
+    public static final String LOGIN = "root";
+    public static final String PASSWORD = "root";  
+}
